dbfcd81ce739f0916fcf33db89f60a5e3cbbf621
kfless12 <kfless12@gmail.com>
2021-04-24 11:41:26 -0400

    first commit for the master branch, setup npm init and prettier and eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+"extends": ["airbnb", "plugin:prettier/recommended"],
+    "plugins": ["jest"],
+    "rules": {
+      "prettier/prettier": [
+        "error",
+        {
+          "singleQuote": true,
+          "semi": false,
+            "endOfLine": "auto"
+        }
+      ],
+      "no-plusplus": "off",
+      "max-classes-per-file": "off",
+      "react/jsx-filename-extension": "off",
+      "react/prefer-stateless-function": "off",
+      "react/state-in-constructor": "off",
+      "import/prefer-default-export": "off"
+    },
+    "env": {
+      "jest/globals": true,
+      "browser": true,
+      "node": true
+    },
+    "parser": "babel-eslint"
+  }
