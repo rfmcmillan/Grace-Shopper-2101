31a366e1390fd8611e34b5c5855a452bd8a11a62
YiruDing <71938628+YiruDing@users.noreply.github.com>
2021-04-27 00:28:15 -0400

    Update Product.js

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -38,7 +38,7 @@ const Product = db.define('product', {
         type: STRING,
         validate: {
             isUrl: true,
-        }
+        },
         defaultValue:
             'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F09%2F08%2Feditors-favorite-snacks-around-the-world-FT-MAG0920.jpg',
     },
