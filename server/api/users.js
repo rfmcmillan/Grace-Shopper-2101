6ece58de855c3d3c692a0c46669c35e7b4537e88
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-04-29 21:39:07 -0400

    initial commit

--- server/api/users.js
+++ server/api/users.js
@@ -6,17 +6,25 @@ const {
 } = require('../db')
 
 router.get('/', async (req, res, next) => {
-    const users = await User.findAll({ include: Review })
-    res.send(users)
+    try {
+        const users = await User.findAll({ include: Review })
+        res.send(users)
+    } catch (error) {
+        next(error)
+    }
 })
 //get single user by specifying their email address
 router.get('/:id', async (req, res, next) => {
-    const user = await User.findOne({
-        where: {
-            id: req.params.id,
-        },
-    })
-    res.send(user)
+    try {
+        const user = await User.findOne({
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
