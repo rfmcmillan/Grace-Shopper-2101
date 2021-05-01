dc65c2acbb7b567f205c3edec2fdbfdd14a2a41c
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-01 15:06:35 -0400

    small commit before merge from main

--- client/store/index.js
+++ client/store/index.js
@@ -1,8 +1,9 @@
-// Create Store Here
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import axios from 'axios';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+
+const LOAD_USERS = 'LOAD_USERS';
 
 //Create Action Creators & Thunks
 
@@ -11,6 +12,7 @@ import logger from 'redux-logger';
 //enter different reducers into combineReducers({}) as a key-value pair. e.g.  'products: productsReducer'
 const reducer = combineReducers({});
 
+//Create Store
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
