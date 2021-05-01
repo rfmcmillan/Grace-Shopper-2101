3eb833cc50b831677471417e33a6728a364978a3
Alejandra Villanueva <alejyvilla@gmail.com>
2021-05-01 17:51:47 -0400

    Working on Nav Bar

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
+import Nav from './Nav';
+
 class Main extends React.Component {
   render() {
     return (
       <Router>
-        <div>Welcome To Global Snacker!</div>
+        <div id="container">
+          <Nav />
+        </div>
       </Router>
     );
   }
