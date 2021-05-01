cd57a8cec48870df8e366c3f3b55a12631fa2713
Yi-Ru Ding <dingyr0925@gmail.com>
2021-05-01 10:33:26 -0400

    country & product routes

--- server/api/countries.js
+++ server/api/countries.js
@@ -1,49 +1,49 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
-    models: { Product, Country },
-} = require('../db')
+  models: { Product, Country },
+} = require('../db');
 
 router.get('/', async (req, res, next) => {
-    try {
-        const countries = await Country.findAll()
-        res.status(200).send(countries)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const countries = await Country.findAll();
+    res.status(200).send(countries);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.get('/:id', async (req, res, next) => {
-    try {
-        const country = await Country.findByPk(req.params.id, {
-            include: [Product],
-        })
-        res.send(country)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const country = await Country.findByPk(req.params.id, {
+      include: [Product],
+    });
+    res.send(country);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.delete('/:id', async (req, res, next) => {
-    try {
-        const DeleteCountry = await Country.destroy({
-            where: { id: req.params.id },
-        })
-        if (!DeleteCountry) res.sendStatus(404)
-        else res.sendStatus(204)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const DeleteCountry = await Country.destroy({
+      where: { id: req.params.id },
+    });
+    if (!DeleteCountry) res.sendStatus(404);
+    else res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-    try {
-        const [name, flag] = req.body
-        const createCountry = await Country.create(name, flag)
-        res.status(201).send(createCountry)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const { name, flag } = req.body;
+    const createCountry = await Country.create({name, flag});
+    res.status(201).send(createCountry);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 // router.put('/:id', async (req, res, next) => {
 //     try {
@@ -68,4 +68,4 @@ router.post('/', async (req, res, next) => {
 //     res.status(201).send(newProduct)
 // })
 
-module.exports = router
+module.exports = router;
--- server/api/products.js
+++ server/api/products.js
@@ -1,66 +1,68 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
-    models: { Product, Country },
-} = require('../db')
+  models: { Product, Country },
+} = require('../db');
 
 router.get('/', async (req, res, next) => {
-    try {
-        const products = await Product.findAll({ include: [Country] })
-        res.send(products)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const products = await Product.findAll({ include: [Country] });
+    res.send(products);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.get('/:id', async (req, res, next) => {
-    try {
-        const product = await Product.findByPk(req.params.id)
-        res.send(product)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.send(product);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.delete('/:id', async (req, res, next) => {
-    try {
-        const DeleteProduct = await Product.destroy({
-            where: { id: req.params.id },
-        })
-        if (!DeleteProduct) res.sendStatus(404)
-        else res.sendStatus(204)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const DeleteProduct = await Product.destroy({
+      where: { id: req.params.id },
+    });
+    if (!DeleteProduct) res.sendStatus(404);
+    else res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-    try {
-        const {
-            title,
-            brand,
-            description,
-            price,
-            inventory,
-            imageUrl,
-            location,
-        } = req.body
-        const createProduct = await Product.create({
-            title,
-            brand,
-            description,
-            price,
-            inventory,
-            imageUrl,
-        })
-        const country = await Country.findOrCreate({
-            where: { name: location },
-        })
-        await createProduct.addCountry(country)
-        //Product.addCountry???
-        res.status(201).send(createProduct)
-    } catch (ex) {
-        next(ex)
-    }
-})
+  try {
+    const {
+      title,
+      brand,
+      description,
+      price,
+      inventory,
+      imageUrl,
+      location,
+    } = req.body;
+    const createProduct = await Product.create({
+      title,
+      brand,
+      description,
+      price,
+      inventory,
+      imageUrl,
+    });
+    const [country] = await Country.findOrCreate({
+      where: { name: location },
+    });
+    console.log('!!!!COUNTRY', country);
+    await createProduct.setCountry(country);
+    //Or country[0]
+    //Product.addCountry???
+    res.status(201).send(createProduct);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-module.exports = router
+module.exports = router;
