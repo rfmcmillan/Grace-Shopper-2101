120556718920e0e64755e6192400fe0a549079eb
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-02 15:10:03 -0400

    added createUser thunk

--- client/store/usersStore.js
+++ client/store/usersStore.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const LOAD_USERS = 'LOAD_USERS';
+const CREATE_USER = 'CREATE_USER';
 
 //Create Action Creators & Thunks
 
-//Load Users Action Creator
 const loadUsersActionCreator = (users) => {
   return {
     type: LOAD_USERS,
@@ -20,13 +20,43 @@ const loadUsers = () => {
   };
 };
 
+const createUserActionCreator = (user) => {
+  return {
+    type: CREATE_USER,
+    user,
+  };
+};
+
+//Create User Thunk
+const createUser = (email, password, firstName, lastName, history) => {
+  return async (dispatch) => {
+    const user = await axios.post('api/users', {
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+    dispatch(createUserActionCreator(user));
+  };
+};
+
 //Create Reducers
 const usersReducer = (state = [], action) => {
   if (action.type === LOAD_USERS) {
     state = action.users;
   }
+  if (action.type === CREATE_USER) {
+    state = [...state, action.user];
+  }
   return state;
 };
 
-//Export Thunks As Named Exports
-export { loadUsers, loadUsersActionCreator, LOAD_USERS, usersReducer };
+export {
+  LOAD_USERS,
+  loadUsersActionCreator,
+  loadUsers,
+  CREATE_USER,
+  createUserActionCreator,
+  createUser,
+  usersReducer,
+};
