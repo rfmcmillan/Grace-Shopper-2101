9d15c54df6dd5f6e59911edd932122d2f4d38948
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-02 14:51:12 -0400

    test for userReducer

--- test/redux.js
+++ test/redux.js
@@ -22,27 +22,94 @@ const User = require('../server/db/models/User');
 enzyme.configure({ adapter: new Adapter() });
 
 //  Redux
+describe('Users in Redux Store', function () {
+  describe('loadUsersActionCreator', function () {
+    it('creates an object with `type` and `users`', async function () {
+      await db.sync({ force: true });
+      await Promise.all([
+        User.create({
+          email: 'kevin@snacker.com',
+          password: 'kevin_pw',
+        }),
+        await User.create({
+          email: 'alejandra@snacker.com',
+          password: 'alejandra_pw',
+        }),
+        await User.create({
+          email: 'ding@snacker.com',
+          password: 'ding_pw',
+        }),
+      ]);
 
-describe('loadUsersActionCreator', function () {
-  it('returns a JavaScript Object', async function () {
-    await db.sync({ force: true });
-    await User.create({
-      email: 'kevin@snacker.com',
-      password: 'kevin_pw',
+      const users = await User.findAll();
+
+      const loadUsersAction = loadUsersActionCreator(users);
+      expect(loadUsersAction.type).to.equal(LOAD_USERS);
+      expect(Array.isArray(loadUsersAction.users)).to.be.true;
+      expect(loadUsersAction.users[2].email).to.equal('ding@snacker.com');
     });
-    await User.create({
-      email: 'alejandra@snacker.com',
-      password: 'alejandra_pw',
-    });
-    await User.create({
-      email: 'yiru@snacker.com',
-      password: 'yiru_pw',
+  });
+  describe('Users Reducer', function () {
+    it('returns a new state with the updated `companies`', async function () {
+      await db.sync({ force: true });
+      await Promise.all([
+        User.create({
+          email: 'kevin@snacker.com',
+          password: 'kevin_pw',
+        }),
+        await User.create({
+          email: 'alejandra@snacker.com',
+          password: 'alejandra_pw',
+        }),
+        await User.create({
+          email: 'ding@snacker.com',
+          password: 'ding_pw',
+        }),
+      ]);
+      const users = await User.findAll();
+
+      const initialState = {
+        users: [],
+      };
+
+      const newState = reducer(initialState, {
+        type: LOAD_USERS,
+        users,
+      });
+      // this should have changed:
+      expect(newState.users).to.deep.equal(users);
     });
 
-    const users = await User.findAll();
+    it('does not modify the previous state', async function () {
+      await db.sync({ force: true });
+      await Promise.all([
+        User.create({
+          email: 'kevin@snacker.com',
+          password: 'kevin_pw',
+        }),
+        await User.create({
+          email: 'alejandra@snacker.com',
+          password: 'alejandra_pw',
+        }),
+        await User.create({
+          email: 'ding@snacker.com',
+          password: 'ding_pw',
+        }),
+      ]);
+      const users = await User.findAll();
 
-    const loadUsersAction = loadUsersActionCreator(users);
-    expect(typeof loadUsersAction).to.equal('object');
-    expect(Object.getPrototypeOf(loadUsersAction)).to.equal(Object.prototype);
+      const initialState = {
+        users: [],
+      };
+
+      const newState = reducer(initialState, {
+        type: LOAD_USERS,
+        users,
+      });
+
+      expect(initialState).to.deep.equal({
+        users: [],
+      });
+    });
   });
 });
