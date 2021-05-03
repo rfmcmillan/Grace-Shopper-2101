88e463c18ad159eaf458f908217329b2cbc5b1b5
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-03 17:28:27 -0400

    adjusted store

--- client/store/index.js
+++ client/store/index.js
@@ -2,7 +2,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import axios from 'axios';
 import { usersReducer } from './usersStore';
 import { productReducer } from './products/products';
 import singleProductReducer from './products/singleProduct';
@@ -24,26 +23,6 @@ const loaded = (state = true, action) => {
   }
 };
 
-const LOAD_USERS = 'LOAD_USERS';
-
-//Create Action Creators & Thunks
-
-//Load Users Action Creator
-const loadUsersAC = (users) => {
-  return {
-    type: LOAD_USERS,
-    users,
-  };
-};
-
-//Load Users Thunk
-const loadUsers = () => {
-  return async (dispatch) => {
-    const users = await axios.get('api/users');
-    dispatch(loadUsersAC(users));
-  };
-};
-
 //enter different reducers into combineReducers({}) as a key-value pair. e.g.  'products: productsReducer'
 const reducer = combineReducers({
   users: usersReducer,
@@ -55,4 +34,4 @@ const reducer = combineReducers({
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 export default store;
 //Export Thunks As Named Exports
-export { loadUsers, loaded, reducer };
+export { loaded, reducer };
