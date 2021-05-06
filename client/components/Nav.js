1d1e5d6d3221ae8d48d9e8aec41513d54c1cc614
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-06 12:32:31 -0400

    added admin buttons to nav bar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,17 +1,52 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-undef */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logoutUser } from '../store/loginstate';
 import { resetCart } from '../store/cart';
+
 class Nav extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
+
   render() {
     const home = window.location.hash === 'disabledfornow';
     return home ? (
       <div> </div>
+    ) : this.props.login.email ? (
+      <div className="navBar">
+        <Link to="/">The Global Snacker</Link>
+        <Link to="/products"> Products</Link>
+        <input type="text" placeholder="search" />
+        {this.props.login.email ? (
+          <div>
+            <h5 id="logged">
+              logged in as:
+              {this.props.login.email}
+            </h5>
+            <button
+              onClick={() => {
+                this.props.logOut();
+                this.props.clearCart();
+              }}
+            >
+              logout
+            </button>
+          </div>
+        ) : (
+          <Link to="/login">Log in</Link>
+        )}
+        <Link to="/manage-products">Manage Products</Link>
+        <Link to="/manage-orders">Manage Orders</Link>
+        <Link to="/manage-users">Manage Users</Link>
+        <Link to="/cart">
+          Cart(
+          {this.props.cart.length})
+        </Link>
+      </div>
     ) : (
       <div className="navBar">
         <Link to="/">The Global Snacker</Link>
@@ -47,6 +82,7 @@ const mapStateToProps = (state) => {
   const { login, cart } = state;
   return { login, cart };
 };
+
 const mapDispatchToProps = (dispatch) => {
   return {
     logOut: () => {
