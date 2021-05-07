d844e0e93dc60d89a5167555d92c82d4cbc95755
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-06 21:33:10 -0400

    LogIn page

--- client/components/LogInPage.js
+++ client/components/LogInPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import LogIn from './LogIn.js';
+import LogIn from './LogIn';
 
 const LogInPage = () => {
   return (
